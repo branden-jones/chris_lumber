cc643fa8d06d690edcca44efaf9868bb04efd07a
Branden Jones <branden.wayne.jones@gmail.com>
2023-03-10 14:02:07 -0500

    TDD

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('sanity check', () => {
+    expect(true).toBe(true);
+})
+
+test('renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(<App />).not.toBeNull();
 });
+
+describe('displays phone number and title', () => {
+  test('displays title', () => {
+    render(<App />)
+    const title = screen.getByText(/chris's website/i)
+    expect(title).toBeTruthy()
+    expect(title).not.toBeNull()
+  })
+  test('displays phone number', () => {
+    render(<App />)
+    const phone = screen.getByText(/497-2241/i)
+    const phone1 = screen.getByText(/\(423\) 497-2241/i)
+    expect(phone).toBeTruthy()
+    expect(phone1).toBeTruthy()
+  })
+})
